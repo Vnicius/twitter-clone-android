a015e4d9730f3fefb33f685c81b758240aa6fea9
vnicius <vinicius.matheus252@gmail.com>
2019-09-12 11:30:20 -0300

    Add ViewModel to SearchResult

--- twitter-clone/app/src/main/java/io/github/vnicius/twitterclone/ui/result/SearchResultActivity.kt
+++ twitter-clone/app/src/main/java/io/github/vnicius/twitterclone/ui/result/SearchResultActivity.kt
@@ -10,6 +10,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
 import io.github.vnicius.twitterclone.R
 import io.github.vnicius.twitterclone.ui.common.adapters.ItemClickListener
 import io.github.vnicius.twitterclone.ui.common.adapters.TweetsAdapter
@@ -24,9 +25,9 @@ import twitter4j.Status
 /**
  * SearchResult Activity View
  */
-class SearchResultActivity : AppCompatActivity(), SearchResultContract.View, View.OnClickListener {
+class SearchResultActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val presenter: SearchResultContract.Presenter = SearchResultPresenter()
+    private lateinit var viewModel: SearchResultViewModel
     private lateinit var query: String
     private lateinit var tweetsAdapter: TweetsAdapter
 
@@ -48,14 +49,17 @@ class SearchResultActivity : AppCompatActivity(), SearchResultContract.View, Vie
     }
 
     override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
         setIntent(intent)
         handleIntent(intent)
     }
 
     private fun handleIntent(intent: Intent) {
+        viewModel = ViewModelProviders.of(this)[SearchResultViewModel::class.java]
+
         if (Intent.ACTION_SEARCH == intent.action) {
             intent.getStringExtra(SearchManager.QUERY)?.also {
-                presenter.build(it)
+                viewModel.build(it)
                 tv_search_field_search_label.text = it
                 query = it
                 overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_fade_out)
@@ -63,38 +67,6 @@ class SearchResultActivity : AppCompatActivity(), SearchResultContract.View, Vie
             initState()
             setupTweetsRecyclerView()
         }
-    }
-
-    override fun onClick(view: View) {
-        when (view.id) {
-            rl_search_field.id -> {
-                val intent = Intent(this, SearchableActivity::class.java).apply {
-                    putExtra(SearchableActivity.QUERY, query)
-                }
-
-                startActivity(intent)
-            }
-
-            btn_connection_error_action.id -> {
-                presenter.getDataSourceValue()?.invalidate()
-            }
-        }
-    }
-
-    override fun showLoader() {
-        hideContent()
-        inc_search_result_spinner.visibility = View.VISIBLE
-    }
-
-    override fun showResult() {
-        hideContent()
-        rv_search_result_tweets_list.visibility = View.VISIBLE
-    }
-
-    override fun showNoResult() {
-        hideContent()
-        tv_search_result_no_result_message.visibility = View.VISIBLE
-        tv_search_result_no_result_message.text = getString(R.string.error_message_no_result, query)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -109,18 +81,50 @@ class SearchResultActivity : AppCompatActivity(), SearchResultContract.View, Vie
         return super.onOptionsItemSelected(item)
     }
 
-    override fun showError(message: String) {
-        Toast.makeText(baseContext, message, Toast.LENGTH_LONG).show()
-    }
+    override fun onClick(view: View) {
+        when (view.id) {
+            rl_search_field.id -> {
+                val intent = Intent(this, SearchableActivity::class.java).apply {
+                    putExtra(SearchableActivity.QUERY, query)
+                }
 
-    override fun showConnectionErrorMessage() {
-        hideContent()
-        inc_search_result_connection_error.visibility = View.VISIBLE
+                startActivity(intent)
+            }
+
+            btn_connection_error_action.id -> {
+                viewModel.getDataSourceValue()?.invalidate()
+            }
+        }
     }
 
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.anim_fade_in, R.anim.anim_slide_out_right)
+    }
+
+    private fun showLoader() {
+        hideContent()
+        inc_search_result_spinner.visibility = View.VISIBLE
+    }
+
+    private fun showResult() {
+        hideContent()
+        rv_search_result_tweets_list.visibility = View.VISIBLE
+    }
+
+    private fun showNoResult() {
+        hideContent()
+        tv_search_result_no_result_message.visibility = View.VISIBLE
+        tv_search_result_no_result_message.text = getString(R.string.error_message_no_result, query)
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(baseContext, message, Toast.LENGTH_LONG).show()
+    }
+
+    private fun showConnectionErrorMessage() {
+        hideContent()
+        inc_search_result_connection_error.visibility = View.VISIBLE
     }
 
     private fun hideContent() {
@@ -150,13 +154,13 @@ class SearchResultActivity : AppCompatActivity(), SearchResultContract.View, Vie
             adapter = tweetsAdapter
         }
 
-        presenter.getValue().observe(this, Observer {
+        viewModel.tweetsList.observe(this, Observer {
             tweetsAdapter.submitList(it)
         })
     }
 
     private fun initState() {
-        presenter.getState().observe(this, Observer {
+        viewModel.state.observe(this, Observer {
             when (it) {
                 State.NO_RESULT -> showNoResult()
                 State.ERROR -> showConnectionErrorMessage()
